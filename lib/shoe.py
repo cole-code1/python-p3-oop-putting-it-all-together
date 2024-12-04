e0c4773bda5274451525c4e5850e47244dd38758
cole-code1 <collinskathu313@gmail.com>
2024-12-04 20:55:44 +0300

    initial commit

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+
+    def __init__(self,brand,size):
+        self.size = size
+        self.brand = brand
+    @property
+    def size(self):
+        return self._size
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size, int):
+            self._size = size
+        else:
+            print("size must be an integer")
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+
+shoe = Shoe("Nike", 10)  # Create a new Shoe object
+print(shoe.size)  # Access size, will call the getter
+print(shoe.brand)
+shoe.size = 12  # Use the setter to set a new size
+shoe.cobble()  # Call the cobble method
